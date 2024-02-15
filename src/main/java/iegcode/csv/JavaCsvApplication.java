44963b61596f8a62ab58c5ba99d91c7e0213cf3a
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-02-15 18:49:33 +0700

    first commit

---
+++ src/main/java/iegcode/csv/JavaCsvApplication.java
@@ -0,0 +1,13 @@
+package iegcode.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iegcode/csv/JavaCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package iegcode.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
